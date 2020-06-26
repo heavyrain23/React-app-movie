03843f5e58242d88e56a6869c1bf25856939802f
Alex <failemotions95@gmail.com>
2020-06-26 15:18:26 +0300

    Styled header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import RMDBLogo from './../images/reactMovie_logo.png'
+import TMDBLogo from './../images/tmdb_logo.svg'
+import {StyledHeader,StyledRMDBLogo, StyledTMDBLogo} from './../styles/StyledHeader'
+
+
 
 const Header = () => (
 
-    <div>Header</div>
+    <StyledHeader >
+        <div className = 'header-content'>
+            <StyledRMDBLogo src={RMDBLogo} alt=""/>
+            <StyledTMDBLogo src={TMDBLogo} alt=""/>
+
+        </div>
+    </StyledHeader>
 )
 
 export default Header
