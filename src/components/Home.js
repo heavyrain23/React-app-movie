5cd68e1eea23caaa47df44ec4c5bebdbc9d95402
Alex <failemotions95@gmail.com>
2020-07-02 15:18:20 +0300

    Movies content

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,26 +5,47 @@ import Grid from './elements/Grid'
 import MovieThumb from './elements/MovieThumb'
 import LoadMoreBtn from './elements/LoadMoreBtn'
 import Spinner from './elements/Spinner'
-import {IMAGE_BASE_URL, BACKDROP_SIZE} from './../config'
+import {IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE} from './../config'
 
 import { useHomeFetch} from './hooks/useHomeFetch'
+import NoImage from './images/no_image.jpg'
 
 const Home = () => {
 
-    const [{ state, loading, error}, fetchMovies] = useHomeFetch();
-    console.log(state);
+    const [{ 
+        state: {movies,currentPage, totalPages, heroImage},
+        loading,
+        error},
+        fetchMovies] = useHomeFetch();
+
+
+    const [searchTerm, setSearchTerm] = useState('');
+
 
     if (error) return <div> Something went wrong ... </div>;
-    if (!state.movies[0]) return <Spinner /> ;
+    if (!movies[0]) return <Spinner /> ;
 
 return (
     <>
-        <HeroImage image = {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-                   title = {state.heroImage.original_title}
-                   text = {state.heroImage.overview}/>
+        <HeroImage image = {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+                   title = {heroImage.original_title}
+                   text = {heroImage.overview}/>
         <SearchBar/>
-        <Grid/>
-        <MovieThumb/>
+        <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+            {movies.map(movie => (
+                <MovieThumb 
+                    key={movie.id}
+                    clickable
+                    image={movie.poster_path
+                         ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                        : NoImage
+                    }
+                    movieId= {movie.id}
+                    movieName={movie.original_title}
+                
+                />
+            ))}
+        </Grid>
         <Spinner/>
         <LoadMoreBtn/>
 
